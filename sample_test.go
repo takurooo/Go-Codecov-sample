33133e9654f251a662ef39ef2e7950e6e56c6722
takurooo <35373553+takurooo@users.noreply.github.com>
2023-12-27 19:59:26 +0900

    add table driven tests

--- sample_test.go
+++ sample_test.go
@@ -3,9 +3,19 @@ package sample
 import "testing"
 
 func TestAdd(t *testing.T) {
-	expected := 3
-	result := Add(1, 2)
-	if result != expected {
-		t.Errorf("result:%v expected:%v", result, expected)
+	tests := []struct {
+		name     string
+		left     int
+		right    int
+		expected int
+	}{
+		{name: "1+1", left: 1, right: 1, expected: 2},
+		{name: "1+10", left: 1, right: 10, expected: 11},
+	}
+	for _, test := range tests {
+		result := Add(test.left, test.right)
+		if result != test.expected {
+			t.Errorf("name:%v result:%v expected:%v", test.name, result, test.expected)
+		}
 	}
 }
